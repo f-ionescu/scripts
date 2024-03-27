846daece4f8735df0e5acd05469feeaa2b360b09
Florian Ionescu <ionescu.floriannicolae@gmail.com>
2024-03-27 08:26:52 +0200

    optimized script

--- delete-s3-objects.py
+++ delete-s3-objects.py
@@ -1,5 +1,21 @@
 import boto3
 
-s3 = boto3.resource('s3')
-bucket = s3.Bucket("eatr-traefik-conf-test-763328378904-us-west-2")
-bucket.object_versions..delete()
+def delete_all_object_versions(bucket_name):
+    # Create an S3 resource
+    s3 = boto3.resource('s3')
+    
+    # Get the bucket
+    bucket = s3.Bucket(bucket_name)
+    
+    # Iterate over all object versions and delete them
+    for obj_version in bucket.object_versions.all():
+        obj_version.delete()
+    
+    print("All object versions deleted from the bucket.")
+
+# Get the bucket name from user input or any other source
+bucket_name = input("Enter the S3 bucket name: ")
+
+# Call the function with the dynamic bucket name
+delete_all_object_versions(bucket_name)
+
